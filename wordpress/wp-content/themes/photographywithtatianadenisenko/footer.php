5defb9af17a28d1aaab1d54bdcfef387a51ceab0
Sergey Zametalin <zametsv@gmail.com>
2017-03-19 13:47:13 -0600

    added few packages, started with header

--- wordpress/wp-content/themes/photographywithtatianadenisenko/footer.php
+++ wordpress/wp-content/themes/photographywithtatianadenisenko/footer.php
@@ -1,4 +1,6 @@
-        <footer></footer>
+        <footer class="footer">
+            <p class="footer__copyright">&copy; Photography with Tatiana Denisenko <?php the_time('Y'); ?></p>
+        </footer>
         <?php wp_footer(); ?>
     </body>
 </html>
--- wordpress/wp-content/themes/photographywithtatianadenisenko/header.php
+++ wordpress/wp-content/themes/photographywithtatianadenisenko/header.php
@@ -1,22 +1,22 @@
 <!doctype html>
-    <html <?php bloginfo('language'); ?>>
-        <head>
-            <meta charset="<?php bloginfo('charset'); ?>">
-            <meta name="viewport" content="width=device-width">
-            <title><?php bloginfo('name'); ?></title>
-            <?php wp_head(); ?>
-        </head>
-        <body>
-            <header>
-                <a href="<?php echo site_url(); ?>" class="logo">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" alt="Logo" width="166px" height="22px" />
-                </a>
-                <nav class="nav">
-                    <?php
-                    $args = array(
-                        'theme_location' => 'primary'
-                    );
-                    ?>
-                    <?php wp_nav_menu($args); ?>
-                </nav>
-            </header>
+<html <?php bloginfo('language'); ?>>
+    <head>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta name="viewport" content="width=device-width">
+        <title><?php bloginfo('name'); ?></title>
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        <header class="header">
+            <a href="<?php echo site_url(); ?>" class="header__logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.svg" alt="Logo" width="166px" height="22px" />
+            </a>
+            <nav class="header__nav">
+                <?php
+                $args = array(
+                    'theme_location' => 'primary'
+                );
+                ?>
+                <?php wp_nav_menu($args); ?>
+            </nav>
+        </header>
