700b59d2ba2d2d0c58bec02075c9df5403012db4
Sergey Zametalin <zametsv@gmail.com>
2017-03-19 22:02:38 -0600

    home page almost ready

--- wordpress/wp-content/themes/photographywithtatianadenisenko/footer.php
+++ wordpress/wp-content/themes/photographywithtatianadenisenko/footer.php
@@ -1,4 +1,4 @@
-        <footer class="footer">
+        <footer class="footer footer_small-margin">
             <p class="footer__copyright">&copy; Photography with Tatiana Denisenko <?php the_time('Y'); ?></p>
         </footer>
         <?php wp_footer(); ?>
--- wordpress/wp-content/themes/photographywithtatianadenisenko/index.php
+++ wordpress/wp-content/themes/photographywithtatianadenisenko/index.php
@@ -1,3 +1,5 @@
 <?php get_header(); ?>
-
+  <div class="container container_small-margin">
+    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/main-hero-image.jpg" class="main-hero-image" alt="Vintage style" height="640px" width="960px">
+  </div>
 <?php get_footer(); ?>
